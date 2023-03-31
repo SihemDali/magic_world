005fb16217686f5fcbee661ed0d87a9571838c7b
Sihem Dali <sihem.dali@gmail.com>
2023-03-31 13:55:41 +0200

    Creation views

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/magics', [MagicController::class, 'index'])->name('magics.index');
+Route::get('/magics/create', [MagicController::class, 'create'])->name('magics.create');
+Route::post('/magics', [MagicController::class, 'store'])->name('magics.store');
+Route::get('/magics/{magic}', [MagicController::class, 'show'])->name('magics.show');
+Route::get('/magics/{magic}/edit', [MagicController::class, 'edit'])->name('magics.edit');
+Route::put('/magics/{magic}', [MagicController::class, 'update'])->name('magics.update');
+Route::delete('/magics/{magic}', [MagicController::class, 'destroy'])->name('magics.destroy');
